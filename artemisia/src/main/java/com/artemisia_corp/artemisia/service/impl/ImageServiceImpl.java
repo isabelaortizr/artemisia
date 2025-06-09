70921c9f56ba1932c72f99c91ba2e15da46485bc
lucas <lucasvalcarce03@gmail.com>
2025-06-09 13:22:13 -0400

    service impl imagen sin nada

---
+++ artemisia/src/main/java/com/artemisia_corp/artemisia/service/impl/ImageServiceImpl.java
@@ -0,0 +1,18 @@
+package com.artemisia_corp.artemisia.service.impl;
+
+import com.artemisia_corp.artemisia.entity.dto.product.ImageUploadDto;
+import com.artemisia_corp.artemisia.service.ImageService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageServiceImpl implements ImageService {
+    @Override
+    public void uploadImage(ImageUploadDto dto) {
+
+    }
+
+    @Override
+    public void deleteImage(Long id) {
+
+    }
+}

62b47f492c7759b0b154b8b6db0d16515e05f4e7
Saul <saulm@email.com>
2025-06-26 11:50:30 -0400

    cambios sin importancia en corsfilter y en application.properties

--- artemisia/src/main/java/com/artemisia_corp/artemisia/config/CorsFilter.java
+++ artemisia/src/main/java/com/artemisia_corp/artemisia/config/CorsFilter.java
@@ -28,8 +28,8 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         response.setHeader("Access-Control-Allow-Origin", "*");
 //        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-        response.setHeader("Access-Control-Allow-Headers", "*");
-//        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+//        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         response.setHeader("Access-Control-Allow-Methods", "*");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
